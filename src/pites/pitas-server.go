75913fb026b14624c168e33d5f5663ace5dea5b2
Antonio J. Sánchez-Padial <antonio@spadial.com>
2020-11-04 22:50:07 +0100

    Lanzado Servidor en escucha

--- src/pites/pitas-server.go
+++ src/pites/pitas-server.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+)
 
 const (
 	connHost = "*"
@@ -10,5 +13,13 @@ const (
 
 func main() {
 	fmt.Println("Arrancando P!TAS Server (" + connType +") on " + connHost + ":" + connPort)
-	
+	l, err := net.Listen(connType, connHost+":"+connPort)
+	if err != nil {
+		fmt.Println("Error en escucha:", err.Error())
+	}
+	defer l.Close();
+
+	for {
+
+	}
 }
